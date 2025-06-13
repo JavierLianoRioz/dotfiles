f3178ddd2ad1cecb6220be3baa103d3c9ce6e091
midas@tower.nixos <javier.liano@alumnos.uneatlantico.es>
2025-06-13 15:43:38 +0200

    Add NixOS configuration modules for system, network, localization, services, users, and packages

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,13 +1,13 @@
 # modules/nixos/default.nix
 {
   imports = [
-    ./boot.nix
-    ./network.nix
-    ./localization.nix
-    ./services.nix
-    ./users.nix
-    ./programs.nix
-    ./packages.nix
-    ./nix.nix
+    ./system/boot.nix
+    ./system/nix.nix
+    ./network/network.nix
+    ./localization/localization.nix
+    ./users/users.nix
+    ./programs/programs.nix
+    ./packages/packages.nix
+    ./services/services.nix
   ];
 }
